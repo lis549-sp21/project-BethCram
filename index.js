db9a87832dfe1e7f12badfcb581c3d478704f798
BethCram <102839011+BethCram@users.noreply.github.com>
2022-06-08 18:16:46 -0700

    Troubleshoot image grid

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-$('img').css("curser","pointer");
+$('img').css("curser", "pointer");
 /* followed tutorial at https://codepen.io/Eric-Wallen/pen/mwydqL for time of day welcome message*/
 var today = new Date();
 var HourNow = today.getHours();
 var greeting;
 var icon;
 
-if (HourNow < 12){
+if (HourNow < 12) {
     greeting = "Good morning! Have an awesome adventure";
     icon = "coffee";
 }
-else if (HourNow < 20){
+else if (HourNow < 20) {
     greeting = "Afternoon! Drink water";
     icon = "water";
 }
@@ -21,3 +21,11 @@ else {
     greeting = "Welcome!";
     icon = "favicon"
 }
+/*
+<input type="img" id="click"></input>
+<button onclick="one()">1</button>
+<button onclick="two()">2</button>
+<button onclick="three()">3</button>
+<script>
+
+</script> */
